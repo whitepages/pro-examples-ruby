cac56d73372e286257adba42663db2a3941cbf9e
Sergei Akulich <sakulich@gmail.com>
2016-12-21 15:17:38 -0800

    Upgrade Lead Verify to version 3.3
    add country_code request param, update version, remove name checks

--- lead_verify.rb
+++ lead_verify.rb
@@ -8,16 +8,17 @@ params = {
   'address_city' => 'Ashland',
   'address.postal_code' => '59004',
   'address.state_code' => 'MT',
+  'address.country_code' => 'US',
   'address.street_line_1' => '302 Gorham Ave',
   'ip_address' => '108.194.128.165',
   'api_key' => ENV['LEAD_VERIFY_API_KEY']
 }
 
-uri = URI('https://proapi.whitepages.com/3.1/lead_verify.json')
+uri = URI('https://proapi.whitepages.com/3.3/lead_verify.json')
 uri.query = URI.encode_www_form(params)
 
 response_hash = JSON.parse(open(uri).read)
 
-available_checks = %w(name_checks phone_checks address_checks email_address_checks ip_address_checks)
+available_checks = %w(phone_checks address_checks email_address_checks ip_address_checks)
 
 available_checks.each { |check| puts response_hash.fetch(check) }
